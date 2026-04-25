305a87e7792ee56c321dced462a48ddbb1c17f18
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2026-04-24 21:36:08 -0700

    Harden upcoming rides review changes

--- lib/google_directions_api_client.rb
+++ lib/google_directions_api_client.rb
@@ -17,6 +17,8 @@ class GoogleDirectionsApiClient
 
   # GOOGLE_DIRECTIONS_API_ERROR is the error message when the Google Directions API returns an error
   GOOGLE_DIRECTIONS_API_ERROR = "Google Directions API Error"
+  OPEN_TIMEOUT_SECONDS = 5
+  READ_TIMEOUT_SECONDS = 5
 
   # api_key is the Google Directions API key
   def initialize(api_key)
@@ -41,8 +43,11 @@ class GoogleDirectionsApiClient
   end
 
   def send_request(url)
-    JSON.parse(URI.parse(url).open.read)
-  rescue URI::Error
+    JSON.parse(
+      URI.parse(url).open(open_timeout: OPEN_TIMEOUT_SECONDS, read_timeout: READ_TIMEOUT_SECONDS).read
+    )
+  rescue URI::Error, OpenURI::HTTPError, SocketError, Timeout::Error, Errno::ECONNREFUSED, Errno::ETIMEDOUT,
+         JSON::ParserError
     raise DirectionsAPIError, "Unable to fetch directions. Please try again."
   end
 
@@ -52,7 +57,8 @@ class GoogleDirectionsApiClient
   end
 
   def parse_response(response)
-    route = response["routes"].first["legs"].first
+    route = response.dig("routes", 0, "legs", 0)
+    raise DirectionsAPIError, GOOGLE_DIRECTIONS_API_ERROR if route.blank?
 
     [route["distance"]["value"].to_f / METERS_IN_A_MILE, # Convert meters to miles
      route["duration"]["value"].to_f / SECONDS_IN_AN_HOUR] # Convert seconds to hours
