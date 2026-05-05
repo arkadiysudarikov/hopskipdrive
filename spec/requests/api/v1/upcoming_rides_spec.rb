d541cf9576d044a85e4ab4deab4e5188df2a6e0a
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2026-05-05 15:44:04 -0700

    Isolate request spec test data

--- spec/requests/api/v1/upcoming_rides_spec.rb
+++ spec/requests/api/v1/upcoming_rides_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe 'api/v1/upcoming_rides' do
   end
 
   before do
+    Ride.delete_all
+    Driver.delete_all
+    Address.delete_all
+
     ["1588 E Thomspon Blvd",
      "2112 E Thompson Blvd"].each do |address|
       Address.find_or_create_by!(address: address)
